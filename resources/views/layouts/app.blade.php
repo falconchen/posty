22d0b3a38a341efbc9a427145e600b9fec779dee
falcon <falcon_chen@qq.com>
2022-02-23 15:09:36 +0800

    update view and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,34 @@
     <title>Document</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex item-center">
+            <li class="p-3">
+                <a href="">Home</a>
+            </li>
+            <li class="p-3">
+                <a href="">Dashboard</a>
+            </li>
+            <li class="p-3">
+                <a href="">Post</a>
+            </li>
+        </ul>
+        <ul class="flex item-center">
+            <li class="p-3">
+                <a href="">Falcon</a>
+            </li>
+            <li class="p-3">
+                <a href="">Login</a>
+            </li>
+            <li class="p-3">
+                <a href="">Register</a>
+            </li>
+            <li class="p-3">
+                <a href="">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
